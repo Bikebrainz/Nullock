dbbacfd10bcf90c3f0af7a5fca1f299d1ac507c4
Gratonic <189929383+Gratonic@users.noreply.github.com>
2026-04-06 22:30:01 -0700

    got JavaFX working, BIG WIN :)

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# check if the user is in the root directory
+if [[ "$(basename "$PWD")" != "Nullock" ]]; then
+  echo "must be in the root directory (Nullock)" >&2
+  exit 1
+fi
+
+# determine the users architecture
+arch="$(uname -m)"
+
+# create a temporary directory for the JavaFX SDK files
+mkdir -p ./JavaFX
+
+case "$arch" in
+  x86_64|amd64)
+    echo "fetching the JavaFX SDK"
+    wget -O ~/JavaFX/zipped_up_shit.zip 'https://download2.gluonhq.com/openjfx/21.0.10/openjfx-21.0.10_linux-x64_bin-sdk.zip'
+    
+    echo "unpacking the JavaFX SDK files into ./lib"
+    unzip ~/JavaFX/zipped_up_shit.zip -d ~/JavaFX -y 
+    mv ~/javafx-sdk-21.0.10/lib/* ./lib/ 
+esac
+
+rm -rf ./JavaFX
